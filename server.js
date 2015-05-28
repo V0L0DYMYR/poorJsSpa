7e816ef5b3a5637208cc89c5f189456c96237345
volodymyr derkach <vlde@ciklum.com>
2015-05-28 16:11:17 +0300

    template

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('./client');
+var fileServer = new static.Server('./angular-client');
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
